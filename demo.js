6f2d69d23180cc8674e3507306024e42c93045c6
Aitor Ibañez <freelance@aitoribanez.com>
2016-01-27 16:25:01 +0100

    - Initial commit

---
+++ demo.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var app = angular.module('demo', ['ngSanitize', 'ui.select']);
+
+app.controller('DemoCtrl', function($scope, $http, $timeout) {
+  $scope.availableColors = ['Red','Green','Blue','Yellow','Magenta','Maroon','Umbra','Turquoise'];
+
+  $scope.multipleDemo = {};
+  $scope.multipleDemo.colors = ['Blue','Red'];
+});
